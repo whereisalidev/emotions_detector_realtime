97c9bcf5ebeec9ff64bc662ef49a8d950da4bdb1
ali-jee-786 <786syedalihussain@gmail.com>
2024-09-29 10:16:16 +0500

    updated

--- app/camera.py
+++ app/camera.py
@@ -20,7 +20,7 @@ class VideoCamera(object):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (89, 2, 236), 1)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 128, 0), 2)
             face_region = image[y:y + h, x:x + w]
 
             try:
