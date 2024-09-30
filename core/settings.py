ba42a3bd56e339bea45ee2c5a178dab3167a88de
ali-jee-786 <786syedalihussain@gmail.com>
2024-09-30 13:51:11 +0500

    updated

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,8 +123,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
---
+++ staticfiles/script.js
@@ -0,0 +1,50 @@
+function smoothUpdateSlider(slider, targetValue) {
+    let currentValue = parseInt(slider.value);
+    const increment = (targetValue - currentValue) / 10; 
+    const updateInterval = setInterval(() => {
+        currentValue += increment;
+
+        if (Math.abs(currentValue - targetValue) < 1) {
+            currentValue = targetValue;
+            clearInterval(updateInterval);
+        }
+
+        slider.value = currentValue;
+        slider.dispatchEvent(new Event('input'));
+    }, 50); 
+}
+
+function updateSliders(emotions) {
+    smoothUpdateSlider(document.getElementById('happy-slider'), emotions.happy);
+    smoothUpdateSlider(document.getElementById('sad-slider'), emotions.sad);
+    smoothUpdateSlider(document.getElementById('neutral-slider'), emotions.neutral);
+    smoothUpdateSlider(document.getElementById('angry-slider'), emotions.angry);
+    smoothUpdateSlider(document.getElementById('surprise-slider'), emotions.surprise);
+    smoothUpdateSlider(document.getElementById('fear-slider'), emotions.fear);
+
+    document.getElementById('happy-percentage').innerText = parseInt(emotions.happy) + '%';
+    document.getElementById('sad-percentage').innerText = parseInt(emotions.sad) + '%';
+    document.getElementById('neutral-percentage').innerText = parseInt(emotions.neutral) + '%';
+    document.getElementById('angry-percentage').innerText = parseInt(emotions.angry) + '%';
+    document.getElementById('surprise-percentage').innerText = parseInt(emotions.surprise) + '%';
+    document.getElementById('fear-percentage').innerText = parseInt(emotions.fear) + '%';
+}
+
+setInterval(() => {
+    fetch('/get-emotions/')
+        .then(response => response.json())
+        .then(data => {
+            updateSliders(data);
+        })
+        .catch(error => console.error('Error fetching emotions:', error));
+}, 1000);
+
+function updateSliderBackground(slider) {
+    const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.setProperty('--value', `${value}%`);
+}
+
+document.querySelectorAll('input[type="range"]').forEach(slider => {
+    updateSliderBackground(slider); 
+    slider.addEventListener('input', () => updateSliderBackground(slider)); 
+});
